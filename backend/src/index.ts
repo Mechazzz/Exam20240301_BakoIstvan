dd7fdcd89f4eb2d75c67866fdd710a85de9ec4c7
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2024-03-01 19:59:11 +0100

    Exam_solution

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,81 @@
-console.log("hello world")
+import express from "express";
+import cors from "cors";
+import { z } from "zod";
+import fs from "fs/promises";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+type Registration = {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+};
+
+const loadDB = async (filename: string) => {
+  try {
+    const rawData = await fs.readFile(
+      `${__dirname}/../database/${filename}.json`,
+      "utf-8"
+    );
+    const data = JSON.parse(rawData);
+    return data as Registration[];
+  } catch (error) {
+    return null;
+  }
+};
+
+const saveDB = async (filename: string, data: any) => {
+  try {
+    const fileContent = JSON.stringify(data);
+    await fs.writeFile(
+      `${__dirname}/../database/${filename}.json`,
+      fileContent
+    );
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const PostRequest = z.object({
+  email: z.string(),
+  password: z.string(),
+  passwordConfirmation: z.string(),
+});
+
+app.post("/api/userData", async (req, res) => {
+  const result = PostRequest.safeParse(req.body);
+  if (!result.success) return res.status(400).json(result.error.issues);
+  const newUserData = result.data;
+  console.log(newUserData);
+  const allUserData = await loadDB("data");
+  console.log(allUserData);
+  if (!allUserData) return res.sendStatus(500);
+  const emailExists = allUserData.some(
+    (user) => user.email === newUserData.email
+  );
+  if (emailExists) {
+    return res.status(400).json({ error: "Email address already exists" });
+  }
+  if (newUserData.password !== newUserData.passwordConfirmation) {
+    return res.status(400).json({ error: "Passwords are not the same" });
+  }
+  if (newUserData.password.length < 5) {
+    return res
+      .status(400)
+      .json({ error: "Passwords need to be at least 5 character long" });
+  }
+  const isSuccessful = await saveDB("data", [
+    ...allUserData,
+    { ...newUserData },
+  ]);
+
+  if (!isSuccessful) return res.sendStatus(500);
+
+  res.json({ ...newUserData });
+});
+
+app.listen(4500);
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,89 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { RegistrationSchema, safeFetch } from "./http";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const registerButton = document.getElementById(
+  "registrationButton"
+) as HTMLButtonElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const backButton = document.getElementById("backButton") as HTMLButtonElement;
+
+const emailDataField = document.getElementById(
+  "emailInput"
+) as HTMLInputElement;
+
+const passwordDataField = document.getElementById(
+  "passwordInput"
+) as HTMLInputElement;
+
+const passwordConfirmInput = document.getElementById(
+  "passwordConfirmInput"
+) as HTMLInputElement;
+
+passwordDataField.addEventListener("input", () => {
+  if (
+    passwordDataField.value.length < 5 ||
+    passwordDataField.value !== passwordConfirmInput.value
+  ) {
+    passwordDataField.classList.add("input-error");
+    passwordDataField.classList.remove("input-valid");
+  } else if (
+    passwordDataField.value.length >= 5 &&
+    passwordDataField.value === passwordConfirmInput.value
+  ) {
+    passwordDataField.classList.remove("input-error");
+    passwordDataField.classList.add("input-valid");
+  }
+});
+
+passwordConfirmInput.addEventListener("input", () => {
+  if (
+    passwordConfirmInput.value.length < 5 ||
+    passwordDataField.value !== passwordConfirmInput.value
+  ) {
+    passwordConfirmInput.classList.add("input-error");
+    passwordConfirmInput.classList.remove("input-valid");
+  } else if (
+    passwordConfirmInput.value.length >= 5 &&
+    passwordDataField.value === passwordConfirmInput.value
+  ) {
+    passwordConfirmInput.classList.remove("input-error");
+    passwordConfirmInput.classList.add("input-valid");
+  }
+});
+
+let popupElement = document.getElementById("newPage") as HTMLDivElement;
+
+const showNewWindow = () => {
+  popupElement.style.display = "flex";
+};
+
+const hideNewWindow = () => {
+  popupElement.style.display = "none";
+};
+
+registerButton.addEventListener("click", async () => {
+  showNewWindow();
+  await postData();
+});
+
+backButton.addEventListener("click", () => {
+  hideNewWindow();
+});
+
+const postData = async () => {
+  const response = await safeFetch(
+    "POST",
+    `http://localhost:4500/api/userData`,
+    RegistrationSchema,
+    {
+      email: emailDataField.value,
+      password: passwordDataField.value,
+      passwordConfirmation: passwordConfirmInput.value,
+    }
+  );
+  if (response?.status === 200) {
+    alert("Success");
+  } else {
+    alert("Error");
+  }
+};
